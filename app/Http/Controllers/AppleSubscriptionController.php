65a3b869fe1881fa52690a7f4532b5ef418a9601
Eyüp Toprak <etoprak_le@hotmail.com>
2023-05-03 11:26:19 +0300

    apple subscription mocking  as a apple api and stores related data
    In addition, the following operations were performed:
    -  Edited  model
    -  expire_date attribute convert to the carbon object

---
+++ app/Http/Controllers/AppleSubscriptionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AppleSubscription;
+use Illuminate\Http\Request;
+
+class AppleSubscriptionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AppleSubscription $appleSubscription)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AppleSubscription $appleSubscription)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AppleSubscription $appleSubscription)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AppleSubscription $appleSubscription)
+    {
+        //
+    }
+}
