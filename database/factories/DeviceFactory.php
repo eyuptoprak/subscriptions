54b58dc19228450f2957503c29c5d5c872db1e98
Eyüp Toprak <etoprak_le@hotmail.com>
2023-05-03 08:45:23 +0300

    Create device factory

---
+++ database/factories/DeviceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Device>
+ */
+class DeviceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'uuid' => fake()->uuid,
+            'appId' => fake()->domainWord,
+            'language_code' => fake()->languageCode,
+            'operating_system' => fake()->linuxPlatformToken,
+        ];
+    }
+}
