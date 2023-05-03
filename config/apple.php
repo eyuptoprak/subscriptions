83ac8e978b142d7e0f84e9c7917e605c1ab381e1
Eyüp Toprak <etoprak_le@hotmail.com>
2023-05-03 11:43:11 +0300

    Add config file of apple api

---
+++ config/apple.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'api' => [
+        'subscription_check_url'=>'http://apple.subscriptions.test/api/subscriptions',
+        'username'=> 'test',
+        'password'=>'password',
+    ],
+
+];
