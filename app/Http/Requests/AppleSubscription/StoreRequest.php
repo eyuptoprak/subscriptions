f2fa81f8d9b82c72568066e00075ce16bb219a9f
Eyüp Toprak <etoprak_le@hotmail.com>
2023-05-03 11:31:24 +0300

    Create apple subscription store request validation

---
+++ app/Http/Requests/AppleSubscription/StoreRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\AppleSubscription;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'receipt' => 'required|numeric',
+            'username' => 'required',
+            'password' => 'required',
+        ];
+    }
+
+    //TODO: failed validation errors must be called from one place
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
