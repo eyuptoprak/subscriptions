1835c97b0fef9b0a1716d14869e95d303cb33d3b
Eyüp Toprak <etoprak_le@hotmail.com>
2023-05-03 14:08:17 +0300

    Create purchase model, controoller and migration

---
+++ app/Models/Purchase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['receipt','status','client_token', 'appId', 'expire_date'];
+    protected $table = "purchases";
+    protected $guarded = ['id'];
+
+    public function getExpireDateAttribute($date)
+    {
+        return Carbon::parse($date);
+    }
+}
---
+++ database/migrations/2023_05_03_104904_create_purchases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->string('receipt', 150);
+            $table->string('appId', 150);
+            $table->string('client_token', 150);
+            $table->string('store', 150);
+            $table->enum('status', \App\Enums\Purchase\PurchaseStatus::array());
+            $table->dateTimeTz('expire_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
