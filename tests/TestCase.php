e98f37fbba02ca5c6fdc28e826be7b04a8d5c70e
Eyüp Toprak <etoprak_le@hotmail.com>
2023-05-03 08:59:23 +0300

    Change test environment settings

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,34 @@
 
 namespace Tests;
 
+use Exception;
+use Faker\Factory;
+use Faker\Generator;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-abstract class TestCase extends BaseTestCase
-{
-    use CreatesApplication;
+abstract class TestCase extends BaseTestCase {
+
+    use CreatesApplication, DatabaseMigrations;
+
+    use DatabaseTransactions;
+
+    private Generator $faker;
+
+    public function setUp()
+    : void {
+
+        parent::setUp();
+        $this->faker = Factory::create();
+    }
+
+    public function __get($key) {
+
+        if ($key === 'faker')
+            return $this->faker;
+        throw new Exception('Unknown Key Requested');
+    }
 }
