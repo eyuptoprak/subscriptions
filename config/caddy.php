995196c7572a774ad639dc339fc3a8f1d4a39e70
Eyüp Toprak <etoprak_le@hotmail.com>
2023-05-03 15:46:20 +0300

    Implement caddy
    The caddy for local ssl certificate

---
+++ config/caddy.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authorized Domains
+    |--------------------------------------------------------------------------
+    |
+    | Domains that are authorized to be viewed through Caddy.
+    |
+    */
+
+    'authorized' => [
+        'laravel.test',
+        'google.laravel.test',
+        'apple.laravel.test',
+        // 'app.laravel.test',
+    ],
+
+];
